6afca1dffa47dc89a7d122b8469e387e3a25e01a
Jefferson Murillo <development@eticsas.co>
2022-09-10 16:44:13 -0500

    Primeros pasos javascript intro terminado full

--- src/index.js
+++ src/index.js
@@ -1,20 +1,3 @@
 
 
-let firstName = 'Jefferson'
-let lastName
-
-/* console.log(`${firstName || 'No firstName'} ${lastName || 'No lastName'}`); */
-
-const isActive = true
-
-const message = ( !isActive ) ? 'Activo' : 'Inactivo'
-
-/* let message = '' */
-
-/* if ( !isActive ) {
-    message = 'Activo'
-}else {
-    message = 'Inactivo'
-} */
-
-console.log(message);
+console.log('hola mundo!!!');
